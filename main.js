73b36f2818782870ba908840cb06422154beee4a
Levy <levyjoanisanchezrivera@gmail.com>
2025-06-15 19:33:01 -0600

    cambios

--- main.js
+++ main.js
@@ -2,22 +2,6 @@ onload = () => {
   document.body.classList.remove("container");
   const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
-
-      const titleText = 'I LOVE YOU'; // Texto que quieres mostrar
-      const titleElement = document.getElementById('title');
-      titleElement.innerHTML = ''; // Limpia el contenido previo (por si acaso)
-      let index = 0;
-
-      function appendTitle() {
-          if (index < titleText.length) {
-              titleElement.innerHTML += titleText[index];
-              index++;
-              setTimeout(appendTitle, 300); // Retraso de 300ms entre letras
-          }
-      }
-
-      appendTitle();
-
       clearTimeout(c);
   }, 1000);
 };
